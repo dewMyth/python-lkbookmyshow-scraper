3d6ecc83ad0f83c6b3c384780f7ff92ecdf3f879
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-10-26 11:20:46 +0530

    Change scheduler

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 from logger import logger
 from scraper import scrape_movies
 from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
 
-scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=1, name='scrape_scheduler')
+scheduler = BlockingScheduler(timezone=pytz.timezone('Asia/Kolkata'))  # GMT+5:30
+
 def start_app():
     scrape_movies()
 
+scheduler.add_job(start_app, 'cron', minute='*/1')
 
 if __name__ == '__main__':
     logger.info("Scheduler starting...")

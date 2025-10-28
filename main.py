0c43c79449ba3d9c81862398f4dcc5e6dbe0d7f5
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-10-28 12:50:38 +0530

    change cron time

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ scheduler = BlockingScheduler(timezone=pytz.timezone('Asia/Kolkata'))  # GMT+5:3
 # )
 
 # Run every hour
-@scheduler.scheduled_job('interval', minutes=1)
+@scheduler.scheduled_job('interval', minutes=5)
 def start_app():
     logger.info("Started Job...")
     try:
